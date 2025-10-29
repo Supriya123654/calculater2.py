8385b0d2f1a7fdf88f8d3a1a0008fbc8d94fc901
supriya.divanad <01fe24bca221@kletech.ac.in>
2025-10-29 11:26:22 +0530

    Arithmatic operations

---
+++ Mathoperation.py
@@ -0,0 +1,24 @@
+# Program to perform addition and subtraction
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing addition
+addition = num1 + num2
+
+# Performing subtraction
+subtraction = num1 - num2
+
+# performing multiplication
+multiplication = num1 * num2
+
+# performing division
+division = num1 / num2
+
+
+# Displaying results
+print("The sum of", num1, "and", num2, "is:", addition)
+print("The difference between", num1, "and", num2, "is:", subtraction)
+print("The product between", num1, "and", num2, "is:", multiplication)
+print("The division between", num1, "and", num2, "is:", division)
