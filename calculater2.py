224e9803298b8afd365a4a9e60f75e3733e7f707
supriya.divanad <01fe24bca221@kletech.ac.in>
2025-10-29 09:56:36 +0530

    first commit

---
+++ calculater2.py
@@ -0,0 +1,15 @@
+# Program to perform addition and subtraction
+
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Performing addition
+addition = num1 + num2
+
+# Performing subtraction
+subtraction = num1 - num2
+
+# Displaying results
+print("The sum of", num1, "and", num2, "is:", addition)
+print("The difference between", num1, "and", num2, "is:", subtraction)
